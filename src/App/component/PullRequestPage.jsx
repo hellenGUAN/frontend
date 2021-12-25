70dd8b4a3caf1247f14a7263d9bfd65db70a3302
上官昌華 <hellen.sk0115@gmail.com>
2021-12-25 22:17:32 +0800

    fix chart data bug

--- src/App/component/PullRequestPage.jsx
+++ src/App/component/PullRequestPage.jsx
@@ -1,11 +1,11 @@
-import {useEffect, useState} from 'react'
-import {makeStyles} from '@material-ui/core/styles'
+import { useEffect, useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import ProjectAvatar from './ProjectAvatar'
 import DrawingBoard from './DrawingBoard'
 import Axios from 'axios'
 import moment from 'moment'
-import {Backdrop, CircularProgress} from '@material-ui/core'
-import {connect} from 'react-redux';
+import { Backdrop, CircularProgress } from '@material-ui/core'
+import { connect } from 'react-redux';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -25,7 +25,7 @@ const useStyles = makeStyles((theme) => ({
 function PullRequestsPage(prop) {
   const classes = useStyles()
   const [pullRequestListData, setPullRequestListData] = useState([])
-  const [dataForPullRequestChart, setDataForPullRequestChart] = useState({labels: [], data: {opened: [], closed: [], merged: []}})
+  const [dataForPullRequestChart, setDataForPullRequestChart] = useState({ labels: [], data: { opened: [], closed: [], merged: [] } })
 
   const [currentProject, setCurrentProject] = useState({})
 
@@ -42,109 +42,96 @@ function PullRequestsPage(prop) {
 
   useEffect(() => {
     Axios.get(`http://localhost:9100/pvs-api/project/1/${projectId}`,
-      {headers: {"Authorization": `${jwtToken}`}})
+      { headers: { "Authorization": `${jwtToken}` } })
       .then((response) => {
         setCurrentProject(response.data)
       })
       .catch((e) => {
-        alert(e.response.status)
+        alert(e.response?.status)
         console.error(e)
       })
   }, [])
 
-  useEffect(() => {
-    if (Object.keys(currentProject).length !== 0) {
-      handleToggle()
-      const githubRepo = currentProject.repositoryDTOList.find(repo => repo.type === 'github')
+  const getPullRequestsFromGitHub = () => {
+    const githubRepo = currentProject.repositoryDTOList.find(repo => repo.type === 'github')
+    if (githubRepo !== undefined) {
       const query = githubRepo.url.split("github.com/")[1]
 
       // todo need reafctor with async
       Axios.get(`http://localhost:9100/pvs-api/github/pullRequests/${query}`,
-        {headers: {"Authorization": `${jwtToken}`}})
+        { headers: { "Authorization": `${jwtToken}` } })
         .then((response) => {
-          if(response.data !== undefined) {
+          if (response?.data) {
             setPullRequestListData(response.data)
           }
-          handleClose()
         })
         .catch((e) => {
-          alert(e.response.status);
+          alert(e.response?.status);
           console.error(e)
         })
     }
-  }, [currentProject, prop.startMonth, prop.endMonth])
+  }
 
   useEffect(() => {
-    const {endMonth} = prop
-    let chartDataset = {labels: [], data: {merged: [], closed: [], opened: []}}
-    const closedPullRequestListData = []
-    const mergedPullRequestListData = []
-    for(const pr in pullRequestListData) {
-      if(pr !== null) {
-        console.log(pr)
-        closedPullRequestListData.push(pr)
-      }
-      if(pr.mergedAt !== null) {
-        mergedPullRequestListData.push(pr)
-      }
+    if (Object.keys(currentProject).length !== 0) {
+      handleToggle()
+      getPullRequestsFromGitHub()
+      handleClose()
+    }
+  }, [currentProject, prop.startMonth, prop.endMonth])
+
+  // Generate the pull-request chart
+  useEffect(() => {
+    const { startMonth, endMonth } = prop
+    let chartDataset = { labels: [], data: { merged: [], closed: [], opened: [] } }
+    let pullRequestListDataSortedByCreatedAt = pullRequestListData
+    let pullRequestListDataSortedByClosedAt = pullRequestListData
+    let pullRequestListDataSortedByMergedAt = pullRequestListData
+
+    // Sort data by date
+    if (pullRequestListData !== undefined) {
+      [].slice.call(pullRequestListDataSortedByCreatedAt).sort((a, b) => a.createdAt - b.createdAt);
+      [].slice.call(pullRequestListDataSortedByClosedAt).sort((a, b) => a.closedAt - b.closedAt);
+      [].slice.call(pullRequestListDataSortedByMergedAt).sort((a, b) => a.mergedAt - b.mergedAt);
     }
 
-    pullRequestListData.sort((a, b) => a.createdAt - b.createdAt)
-    closedPullRequestListData.sort((a, b) => a.closedAt - b.closedAt)
-    mergedPullRequestListData.sort((a, b) => a.mergedAt - b.mergedAt)
-
-    console.log(closedPullRequestListData)
-
-    if (pullRequestListData.length > 0) {
-      for (let month = moment(pullRequestListData[0].createdAt); month < moment(endMonth).add(1, 'months'); month = month.add(1, 'months')) {
+    if (pullRequestListDataSortedByCreatedAt.length > 0) {
+      // Calculate the number of pull requests for each month in the selected range
+      for (let month = moment(startMonth); month < moment(endMonth).add(1, 'months'); month = month.add(1, 'months')) {
         let index
+        let noCloseCount = 0  // Number of pull requests without 'closedAt' date
+        let noMergeCount = 0  // Number of pull requests without 'mergedAt' date
         chartDataset.labels.push(month.format("YYYY-MM"))
 
-        index = pullRequestListData.findIndex(pullRequest => {
-          return moment(pullRequest.createdAt).format("YYYY-MM") === month.format("YYYY-MM")
+        index = pullRequestListDataSortedByCreatedAt.findIndex(pullRequest => {
+          return moment(pullRequest.createdAt).year() > month.year() || moment(pullRequest.createdAt).year() === month.year() && moment(pullRequest.createdAt).month() > month.month()
         })
-        chartDataset.data.opened.push(index === pullRequestListData.length ? 0 : index)
+        chartDataset.data.opened.push(index === -1 ? pullRequestListData.length : index)
 
-        index = closedPullRequestListData.findIndex(pullRequest => {
-          return moment(pullRequest.closedAt).format("YYYY-MM") === month.format("YYYY-MM")
+        index = pullRequestListDataSortedByClosedAt.findIndex(pullRequest => {
+          if (pullRequest.closedAt == null) {
+            noCloseCount += 1
+          }
+          return moment(pullRequest.closedAt).year() > month.year() || moment(pullRequest.closedAt).year() === month.year() && moment(pullRequest.closedAt).month() > month.month()
         })
-        chartDataset.data.closed.push(index === -1 ? pullRequestListData.length : index)
+        chartDataset.data.closed.push(index === -1 ? pullRequestListData.length - noCloseCount : index)
 
-        index = mergedPullRequestListData.findIndex(pullRequest => {
-          return moment(pullRequest.mergedAt).format("YYYY-MM") === month.format("YYYY-MM")
+        index = pullRequestListDataSortedByMergedAt.findIndex(pullRequest => {
+          if (pullRequest.mergedAt == null) {
+            noMergeCount += 1
+          }
+          return moment(pullRequest.mergedAt).year() > month.year() || moment(pullRequest.mergedAt).year() === month.year() && moment(pullRequest.mergedAt).month() > month.month()
         })
-        chartDataset.data.merged.push(index === -1 ? pullRequestListData.length : index)
+        chartDataset.data.merged.push(index === -1 ? pullRequestListData.length - noMergeCount : index)
       }
     }
-
-//     for (let month = moment(pullRequestListData[0].createdAt); month <= moment(endMonth).add(1, 'months'); month = month.add(1, 'months')) {
-//       let index
-//       chartDataset.labels.push(month.format("YYYY-MM"))
-//
-//       index = pullRequestListData.findIndex(pullRequest => {
-//         return moment(pullRequest.createdAt).year() >= month.year() && moment(pullRequest.createdAt).month() > month.month()
-//       })
-//       chartDataset.data.opened.push(index === -1 ? pullRequestListData.length : index)
-//
-//       index = pullRequestListData.findIndex(pullRequest => {
-//         return pullRequest.closedAt != null && (moment(pullRequest.closedAt).year() > month.year() || moment(pullRequest.closedAt).year() === month.year()) && moment(pullRequest.closedAt).month() > month.month()
-//       })
-//       chartDataset.data.closed.push(index === -1 ? pullRequestListData.length : index)
-//
-//       index = pullRequestListData.findIndex(pullRequest => {
-//         return pullRequest.mergedAt != null && (moment(pullRequest.mergedAt).year() > month.year() || moment(pullRequest.mergedAt).year() === month.year()) && moment(pullRequest.mergedAt).month() > month.month()
-//       })
-//       chartDataset.data.merged.push(index === -1 ? pullRequestListData.length : index)
-//     }
-
-    console.log(pullRequestListData)
     setDataForPullRequestChart(chartDataset)
-  }, [pullRequestListData, prop.endMonth])
+  }, [pullRequestListData, prop.startMonth, prop.endMonth])
 
   return (
-    <div style={{marginLeft: "10px"}}>
+    <div style={{ marginLeft: "10px" }}>
       <Backdrop className={classes.backdrop} open={open}>
-        <CircularProgress color="inherit"/>
+        <CircularProgress color="inherit" />
       </Backdrop>
       <div className={classes.root}>
         <ProjectAvatar
@@ -153,15 +140,14 @@ function PullRequestsPage(prop) {
         />
         <p>
           <h2>{currentProject.projectName}</h2>
-
         </p>
       </div>
       <div className={classes.root}>
-        <div style={{width: "67%"}}>
+        <div style={{ width: "67%" }}>
           <div>
             <h1>Team</h1>
             <div>
-              <DrawingBoard data={dataForPullRequestChart} color='skyblue' id="team-issue-chart" isIssue={true}/>
+              <DrawingBoard data={dataForPullRequestChart} color='skyblue' id="team-pull-request-chart" isIssue={true} />
             </div>
           </div>
         </div>
@@ -172,6 +158,7 @@ function PullRequestsPage(prop) {
 
 const mapStateToProps = (state) => {
   return {
+    startMonth: state.selectedMonth.startMonth,
     endMonth: state.selectedMonth.endMonth
   }
 }
